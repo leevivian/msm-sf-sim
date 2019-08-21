549636a0fae2bcec438168bb886874d5b613f7a0
Vivian Lee <vivianlee8295@gmail.com>
2019-08-20 18:19:56 -0700

    Add remaining enhnacement values; add cumulative sums to simulate weighted probabilities

--- simulate.js
+++ simulate.js
@@ -21,25 +21,35 @@ checkValidStarInput();
 
 var enhancementTable = new Map([
     [1, new Map([["success", 100], ["cost", 10000]])],
-    [2, new Map([["success", 95], ["maintain", 5], ["degraded", 0], ["destroyed", 0], ["cost", 15000]])],
-    [3, new Map([["success", 90], ["maintain", 10], ["degraded", 0], ["destroyed", 0], ["cost", 20000]])],
-    [4, new Map([["success", 85], ["maintain", 15], ["degraded", 0], ["destroyed", 0], ["cost", 55000]])],
-    [5, new Map([["success", 80], ["maintain", 20], ["degraded", 0], ["destroyed", 0], ["cost", 100000]])],
-    [6, new Map([["success", 75], ["maintain", 25], ["degraded", 0], ["destroyed", 0], ["cost", 200000]])],
-    [7, new Map([["success", 70], ["maintain", 30], ["degraded", 0], ["destroyed", 0], ["cost", 300000]])],
-    [8, new Map([["success", 65], ["maintain", 35], ["degraded", 0], ["destroyed", 0], ["cost", 400000]])],
-    [9, new Map([["success", 60], ["maintain", 40], ["degraded", 0], ["destroyed", 0], ["cost", 600000]])],
-    [10, new Map([["success", 55], ["maintain", 45], ["degraded", 0], ["destroyed", 0], ["cost", 800000]])],
-    [11, new Map([["success", 50], ["maintain", 35], ["degraded", 10], ["destroyed", 5], ["cost", 1000000]])],
-    [12, new Map([["success", 45], ["maintain", 40], ["degraded", 10], ["destroyed", 5], ["cost", 1400000]])],
-    [13, new Map([["success", 40], ["maintain", 40], ["degraded", 15], ["destroyed", 5], ["cost", 1800000]])],
-    [14, new Map([["success", 35], ["maintain", 45], ["degraded", 15], ["destroyed", 5], ["cost", 2200000]])],
-    [15, new Map([["success", 30], ["maintain", 45], ["degraded", 20], ["destroyed", 5], ["cost", 2600000]])],
-    [16, new Map([["success", 25], ["maintain", 50], ["degraded", 20], ["destroyed", 5], ["cost", 3000000]])],
-    [17, new Map([["success", 20], ["maintain", 50], ["degraded", 25], ["destroyed", 5], ["cost", 3500000]])],
-    [18, new Map([["success", 15], ["maintain", 55], ["degraded", 25], ["destroyed", 5], ["cost", 4000000]])],
-    [19, new Map([["success", 10], ["maintain", 55], ["degraded", 30], ["destroyed", 5], ["cost", 4500000]])],
-    [20, new Map([["success", 5], ["maintain", 60], ["degraded", 30], ["destroyed", 5], ["cost", 5000000]])],
+    [2, new Map([["success", 95], ["maintain", 100], ["degraded", 0], ["destroyed", 0], ["cost", 15000]])],
+    [3, new Map([["success", 90], ["maintain", 100], ["degraded", 0], ["destroyed", 0], ["cost", 20000]])],
+    [4, new Map([["success", 85], ["maintain", 100], ["degraded", 0], ["destroyed", 0], ["cost", 55000]])],
+    [5, new Map([["success", 80], ["maintain", 100], ["degraded", 0], ["destroyed", 0], ["cost", 100000]])],
+    [6, new Map([["success", 75], ["maintain", 100], ["degraded", 0], ["destroyed", 0], ["cost", 200000]])],
+    [7, new Map([["success", 70], ["maintain", 100], ["degraded", 0], ["destroyed", 0], ["cost", 300000]])],
+    [8, new Map([["success", 65], ["maintain", 100], ["degraded", 0], ["destroyed", 0], ["cost", 400000]])],
+    [9, new Map([["success", 60], ["maintain", 100], ["degraded", 0], ["destroyed", 0], ["cost", 600000]])],
+    [10, new Map([["success", 55], ["maintain", 100], ["degraded", 0], ["destroyed", 0], ["cost", 800000]])],
+    [11, new Map([["success", 50], ["maintain", 85], ["degraded", 95], ["destroyed", 100], ["cost", 1000000]])],
+    [12, new Map([["success", 45], ["maintain", 85], ["degraded", 95], ["destroyed", 100], ["cost", 1400000]])],
+    [13, new Map([["success", 40], ["maintain", 80], ["degraded", 95], ["destroyed", 100], ["cost", 1800000]])],
+    [14, new Map([["success", 35], ["maintain", 80], ["degraded", 95], ["destroyed", 100], ["cost", 2200000]])],
+    [15, new Map([["success", 30], ["maintain", 75], ["degraded", 95], ["destroyed", 100], ["cost", 2600000]])],
+    [16, new Map([["success", 25], ["maintain", 75], ["degraded", 95], ["destroyed", 100], ["cost", 3000000]])],
+    [17, new Map([["success", 20], ["maintain", 70], ["degraded", 95], ["destroyed", 100], ["cost", 3500000]])],
+    [18, new Map([["success", 15], ["maintain", 70], ["degraded", 95], ["destroyed", 100], ["cost", 4000000]])],
+    [19, new Map([["success", 10], ["maintain", 65], ["degraded", 95], ["destroyed", 100], ["cost", 4500000]])],
+    [20, new Map([["success", 5], ["maintain", 65], ["degraded", 95], ["destroyed", 100], ["cost", 5000000]])],
+    [21, new Map([["success", 1], ["maintain", 50], ["degraded", 90], ["destroyed", 100], ["cost", 6000000]])],
+    [22, new Map([["success", 1], ["maintain", 50], ["degraded", 90], ["destroyed", 100], ["cost", 7000000]])],
+    [23, new Map([["success", 1], ["maintain", 50], ["degraded", 90], ["destroyed", 100], ["cost", 8000000]])],
+    [24, new Map([["success", 1], ["maintain", 50], ["degraded", 90], ["destroyed", 100], ["cost", 9000000]])],
+    [25, new Map([["success", 1], ["maintain", 50], ["degraded", 90], ["destroyed", 100], ["cost", 10000000]])],
+    [26, new Map([["success", 1], ["maintain", 40], ["degraded", 85], ["destroyed", 100], ["cost", 11800000]])],
+    [27, new Map([["success", 1], ["maintain", 40], ["degraded", 85], ["destroyed", 100], ["cost", 14900000]])],
+    [28, new Map([["success", 1], ["maintain", 40], ["degraded", 85], ["destroyed", 100], ["cost", 18000000]])],
+    [29, new Map([["success", 1], ["maintain", 40], ["degraded", 85], ["destroyed", 100], ["cost", 21100000]])],
+    [30, new Map([["success", 1], ["maintain", 40], ["degraded", 85], ["destroyed", 100], ["cost", 24200000]])],
 ]);
 
 // Getting "lucky" assumes an extra 5% is added to the probability of success, taken away from the
@@ -89,21 +99,29 @@ function simulateEnhancement () {
     var outputLog = document.getElementById('output');
     outputLog.innerHTML = "";
     var starValues = getStarInputs();
-    console.log(starValues);
     var currentMesos = 0;
     var attempts = 1;
     var currentSf = starValues.get('start');
-    while (currentSf < starValues.get('end')) {
+    var destroyed = false;
+    while (currentSf < starValues.get('end') && !destroyed) {
         var nextEnhancement = enhancementTable.get(currentSf + 1);
-        var randomNum = Math.floor(Math.random() * 100) + 1;
+        var randomNum = Math.floor(Math.random() * 100); // 0 - 99
+        console.log(randomNum);
         var output1 = document.createElement('p');
         output1.innerHTML = "<b>Attempt " + attempts + "</b><br />";
         currentMesos += nextEnhancement.get("cost");
         if (randomNum < nextEnhancement.get("success")) {
             output1.innerHTML += "<span style='color:green'>Success!</span> SF" + currentSf + " => SF" + (currentSf + 1);
             currentSf++;
-        } else {
+        } else if (randomNum < nextEnhancement.get("maintain")) {
             output1.innerHTML += "Maintained! SF" + currentSf + " => SF" + currentSf;
+        } else if (randomNum < nextEnhancement.get("degraded")) {
+            output1.innerHTML += "<span style='color:blue'>Degraded!</span> SF" + currentSf + " => SF" + (currentSf - 1);
+            currentSf--;
+        } else if (randomNum < nextEnhancement.get("destroyed")) {
+            output1.innerHTML += "<span style='color:red'>Destroyed!</span> SF" + currentSf;
+            destroyed =  true;
+            console.log(document.getElementById('continue').checked);
         }
 
         output1.innerHTML += "<br />" + "Total Mesos: " + currentMesos.toLocaleString() + "<br />";
